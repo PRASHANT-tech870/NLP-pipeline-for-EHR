74bf29ece7a1835db466e7c638b355339fdd049b
Nikhil_Vasu <nikhilvasu4625@gmail.com>
2024-04-09 21:39:17 +0530

    Update inception.py

--- BRAIN MRI/inception.py
+++ BRAIN MRI/inception.py
@@ -1,1 +1,261 @@
+import keras
 
+import keras.backend as K
+import tensorflow as tf
+
+
+from keras.models import Model
+from keras.layers import Conv2D, MaxPool2D, Dropout, Dense, Input, concatenate, GlobalAveragePooling2D, AveragePooling2D, Flatten
+
+
+import numpy as np
+
+from keras import backend as K
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import math
+from keras.optimizers import SGD
+from keras.callbacks import LearningRateScheduler
+
+
+
+train_datagen =ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2,
+    rotation_range=15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+
+)
+
+train_generator=train_datagen.flow_from_directory(
+    'D:\BRAIN PROJECT\Archive_tumor_kaggle\Training',
+    target_size=(224,224),
+    batch_size=32,
+    class_mode='categorical',
+    subset='training'
+)
+
+validation_generator=train_datagen.flow_from_directory(
+    'D:\BRAIN PROJECT\Archive_tumor_kaggle\Validation',
+    target_size=(224,224),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation'
+
+)
+
+test_datagen=ImageDataGenerator(rescale=1./255)
+
+test_generator=test_datagen.flow_from_directory(
+    'D:\BRAIN PROJECT\Archive_tumor_kaggle\Testing',
+    target_size=(224,224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+def inception_module(x,
+                     filters_1x1,
+                     filters_3x3_reduce,
+                     filters_3x3,
+                     filters_5x5_reduce,
+                     filters_5x5,
+                     filters_pool_proj,
+                     name=None):
+    conv_1x1 = Conv2D(filters_1x1, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init,
+                      bias_initializer=bias_init)(x)
+
+    conv_3x3 = Conv2D(filters_3x3_reduce, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init,
+                      bias_initializer=bias_init)(x)
+    conv_3x3 = Conv2D(filters_3x3, (3, 3), padding='same', activation='relu', kernel_initializer=kernel_init,
+                      bias_initializer=bias_init)(conv_3x3)
+
+    conv_5x5 = Conv2D(filters_5x5_reduce, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init,
+                      bias_initializer=bias_init)(x)
+    conv_5x5 = Conv2D(filters_5x5, (5, 5), padding='same', activation='relu', kernel_initializer=kernel_init,
+                      bias_initializer=bias_init)(conv_5x5)
+
+    pool_proj = MaxPool2D((3, 3), strides=(1, 1), padding='same')(x)
+    pool_proj = Conv2D(filters_pool_proj, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init,
+                       bias_initializer=bias_init)(pool_proj)
+
+    output = concatenate([conv_1x1, conv_3x3, conv_5x5, pool_proj], axis=3, name=name)
+
+    return output
+
+kernel_init = keras.initializers.glorot_uniform()
+bias_init = keras.initializers.Constant(value=0.2)
+
+input_layer = Input(shape=(224, 224, 3))
+
+x = Conv2D(64, (7, 7), padding='same', strides=(2, 2), activation='relu', name='conv_1_7x7/2', kernel_initializer=kernel_init, bias_initializer=bias_init)(input_layer)
+x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_1_3x3/2')(x)
+x = Conv2D(64, (1, 1), padding='same', strides=(1, 1), activation='relu', name='conv_2a_3x3/1')(x)
+x = Conv2D(192, (3, 3), padding='same', strides=(1, 1), activation='relu', name='conv_2b_3x3/1')(x)
+x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_2_3x3/2')(x)
+
+x = inception_module(x,
+                     filters_1x1=64,
+                     filters_3x3_reduce=96,
+                     filters_3x3=128,
+                     filters_5x5_reduce=16,
+                     filters_5x5=32,
+                     filters_pool_proj=32,
+                     name='inception_3a')
+
+x = inception_module(x,
+                     filters_1x1=128,
+                     filters_3x3_reduce=128,
+                     filters_3x3=192,
+                     filters_5x5_reduce=32,
+                     filters_5x5=96,
+                     filters_pool_proj=64,
+                     name='inception_3b')
+
+x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_3_3x3/2')(x)
+
+x = inception_module(x,
+                     filters_1x1=192,
+                     filters_3x3_reduce=96,
+                     filters_3x3=208,
+                     filters_5x5_reduce=16,
+                     filters_5x5=48,
+                     filters_pool_proj=64,
+                     name='inception_4a')
+
+x1 = AveragePooling2D((5, 5), strides=3)(x)
+x1 = Conv2D(128, (1, 1), padding='same', activation='relu')(x1)
+x1 = Flatten()(x1)
+x1 = Dense(1024, activation='relu')(x1)
+x1 = Dropout(0.7)(x1)
+x1 = Dense(4, activation='softmax', name='auxilliary_output_1')(x1)
+
+x = inception_module(x,
+                     filters_1x1=160,
+                     filters_3x3_reduce=112,
+                     filters_3x3=224,
+                     filters_5x5_reduce=24,
+                     filters_5x5=64,
+                     filters_pool_proj=64,
+                     name='inception_4b')
+x = inception_module(x,
+                     filters_1x1=128,
+                     filters_3x3_reduce=128,
+                     filters_3x3=256,
+                     filters_5x5_reduce=24,
+                     filters_5x5=64,
+                     filters_pool_proj=64,
+                     name='inception_4c')
+
+x = inception_module(x,
+                     filters_1x1=112,
+                     filters_3x3_reduce=144,
+                     filters_3x3=288,
+                     filters_5x5_reduce=32,
+                     filters_5x5=64,
+                     filters_pool_proj=64,
+                     name='inception_4d')
+
+x2 = AveragePooling2D((5, 5), strides=3)(x)
+x2 = Conv2D(128, (1, 1), padding='same', activation='relu')(x2)
+x2 = Flatten()(x2)
+x2 = Dense(1024, activation='relu')(x2)
+x2 = Dropout(0.7)(x2)
+x2 = Dense(4, activation='softmax', name='auxilliary_output_2')(x2)
+
+x = inception_module(x,
+                     filters_1x1=256,
+                     filters_3x3_reduce=160,
+                     filters_3x3=320,
+                     filters_5x5_reduce=32,
+                     filters_5x5=128,
+                     filters_pool_proj=128,
+                     name='inception_4e')
+
+x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_4_3x3/2')(x)
+
+x = inception_module(x,
+                     filters_1x1=256,
+                     filters_3x3_reduce=160,
+                     filters_3x3=320,
+                     filters_5x5_reduce=32,
+                     filters_5x5=128,
+                     filters_pool_proj=128,
+                     name='inception_5a')
+
+x = inception_module(x,
+                     filters_1x1=384,
+                     filters_3x3_reduce=192,
+                     filters_3x3=384,
+                     filters_5x5_reduce=48,
+                     filters_5x5=128,
+                     filters_pool_proj=128,
+                     name='inception_5b')
+
+x = GlobalAveragePooling2D(name='avg_pool_5_3x3/1')(x)
+
+x = Dropout(0.4)(x)
+
+x = Dense(4, activation='softmax', name='output')(x)
+
+
+model = Model(input_layer, [x, x1, x2], name='inception_v1')
+
+model.summary()
+
+epochs = 25
+initial_lrate = 0.01
+
+def decay(epoch, steps=100):
+    initial_lrate = 0.01
+    drop = 0.96
+    epochs_drop = 8
+    lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
+    return lrate
+
+sgd = SGD(lr=initial_lrate, momentum=0.9, nesterov=False)
+
+lr_sc = LearningRateScheduler(decay, verbose=1)
+
+model.compile(loss=['categorical_crossentropy', 'categorical_crossentropy', 'categorical_crossentropy'], loss_weights=[1, 0.3, 0.3], optimizer=sgd, metrics=['accuracy'])
+
+history = model.fit(train_generator,
+                    steps_per_epoch=train_generator.samples // 32,
+                    validation_data=validation_generator,
+                    validation_steps=validation_generator.samples // 32,
+                    epochs=25,
+                    callbacks=[lr_sc])
+
+
+predictions = model.predict(test_generator)
+
+# Get the predicted class labels and their probabilities
+predicted_labels = np.argmax(predictions, axis=1)
+predicted_probabilities = np.max(predictions, axis=1)
+
+# Get the true class labels
+true_labels = test_generator.classes
+
+# Get the class label names
+class_indices = train_generator.class_indices
+class_labels = list(class_indices.keys())
+
+# Print the predicted class labels and their probabilities
+for i in range(len(predicted_labels)):
+    print('Image {}:'.format(i+1))
+    print('Predicted class:', class_labels[predicted_labels[i]])
+    print('Probability:', predicted_probabilities[i])
+
+# Create the confusion matrix
+conf_matrix = confusion_matrix(true_labels, predicted_labels)
+
+# Display the confusion matrix as a heatmap
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt='g', xticklabels=class_labels, yticklabels=class_labels)
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.title('Confusion Matrix')
+plt.show()
