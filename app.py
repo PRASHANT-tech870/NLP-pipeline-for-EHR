1c9da288450bd5587dc38612733a47845bf47de4
PRASHANT-tech870 <prashantronad870@gmail.com>
2025-06-11 00:31:52 +0530

    model change

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ Format the response in a clear, medical professional style."""
                     "content": prompt,
                 }
             ],
-            model="mixtral-8x7b-32768",
+            model="llama-3.3-70b-versatile",
             temperature=0.1,
         )
         
